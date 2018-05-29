177f8ce9deba0bc21cff37999abcba3d2dc6dab9
Cicilia Lee <aacs0130@gmail.com>
2018-05-29 23:30:54 +0800

    May29test First commit, unfinished

---
+++ 2018/python_format.py
@@ -0,0 +1,12 @@
+class Solution(Object):
+    def prt(self, sting, values):
+        
+    def __init__(self):
+        
+def main():
+    int temp = 123
+
+    
+if __name__ = '__main__':
+    main()
+
